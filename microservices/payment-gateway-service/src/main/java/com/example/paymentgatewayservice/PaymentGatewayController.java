921b758d8b6331dfad0ef601633706665dd68807
vamshikumar20 <vamshivk2002@gmail.com>
2026-04-11 17:01:02 +0530

    docker build

--- microservices/payment-gateway-service/src/main/java/com/example/paymentgatewayservice/PaymentGatewayController.java
+++ microservices/payment-gateway-service/src/main/java/com/example/paymentgatewayservice/PaymentGatewayController.java
@@ -1,9 +1,11 @@
 package com.example.paymentgatewayservice;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class PaymentGatewayController {
 
     @GetMapping("/")
